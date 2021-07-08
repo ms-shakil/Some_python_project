fb2b89045774b1197ecbcee793f4ae090ede8be2
ms-shakil <mdshakil8796@gmail.com>
2021-07-08 22:50:07 +0600

    This is mad libs project

---
+++ Mad_Libs.py
@@ -0,0 +1,6 @@
+Color = input("Enter a Color:")
+Plural_Noun = input("Enter a Plural Noun:")
+Celebrity = input("Enter a Celebrity Name:")
+
+My_madLibs =f"Rose are {Color}.{Plural_Noun} is blue. I love {Celebrity}!"
+print(My_madLibs)
