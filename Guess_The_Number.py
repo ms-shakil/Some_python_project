68b648a50db0b3dfa85e72d9ba3687fb0bcd05c2
ms-shakil <mdshakil8796@gmail.com>
2021-07-09 23:46:23 +0600

    Guess the number

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -1,6 +1,7 @@
 import random
 
-def Guess(x):
+# guess number for user
+def Guess_User(x):
     random_number = random.randint(1,x)
     guess = 0
     while guess != random_number:
@@ -12,4 +13,24 @@ def Guess(x):
     print(f"wow,congrats. You have guessed the number {guess} correctly .")            
     
     
-Guess(10)    
+Guess_User(10)   
+ 
+# guess number for computer 
+def Computer_Guess(x):
+    low = 1
+    high= x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low    
+        feedback = input(f"Is {guess} to high (H), to Low (L) .or correct (C)?").lower()
+        
+        if feedback == "h":
+            high = guess -1
+        elif feedback =="l":
+            low = guess +1
+    print(f"The computer guessed  your number{guess} ,correctly")            
+    
+Computer_Guess(10)    
